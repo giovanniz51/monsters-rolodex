20b2bcd91b73851dd2d9875df26d39d103a33b94
EC2 Default User <ec2-user@ip-172-31-27-14.eu-west-1.compute.internal>
2019-09-17 07:06:42 +0000

    Updates

---
+++ precache-manifest.0b7978a97ea1a221a6cbfe8c6103b1e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5014d97de0dc73dc5d7e33ec3af1615e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c96733d06e18307687a",
+    "url": "/monsters-rolodex/static/css/main.df212c5d.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "6c96733d06e18307687a",
+    "url": "/monsters-rolodex/static/js/main.838eecb2.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
